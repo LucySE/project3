70f62e384afc8b0954a02a5e6b17e7c27db0ca5e
LucySE <lucywirth@gmail.com>
2021-01-31 10:02:06 +1100

    dogs and parks data - seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,16 @@ vet: 'Wooferines',
 vaccination_certificate: "https://res.cloudinary.com/do9y9tnbr/image/upload/v1612003377/dogpark_vaccinations/Hueysvaccinations_mncxki.pdf",
 username: "labradore"
 )
+d1 = Dogpark.create!(
+  capacity: '6',
+  latitude: '-33.9068',
+  longitude: '151.171858'
+)
+b1 = Booking.create!(
+  user_id: '1',
+  park_id: '2',
+  booking_start_date: '2021-06-02 10:00',
+  booking_end_date: '2021-06-02 11:00',
+  report_card: 'A very good boy',
+  photo: 'https://unsplash.com/photos/PUY5xUszd3Y'
+)
