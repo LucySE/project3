e94011d3d43ccc9c9c2b422c6897030476d40c2d
LucySE <lucywirth@gmail.com>
2021-01-30 19:26:38 +1100

    created models

---
+++ db/schema.rb
@@ -0,0 +1,51 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_01_30_080410) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "park_id"
+    t.datetime "booking_start_date"
+    t.datetime "booking_end_date"
+    t.text "report_card"
+    t.text "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dogparks", force: :cascade do |t|
+    t.integer "capacity"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "name"
+    t.text "email"
+    t.string "password_digest"
+    t.text "dog_name"
+    t.string "dog_breed"
+    t.boolean "vaccinated"
+    t.text "vet"
+    t.text "vaccination_certificate"
+    t.text "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
