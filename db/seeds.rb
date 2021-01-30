bc6bc2ffd9bc3ce5031c12add8fe0d676a98586b
LucySE <lucywirth@gmail.com>
2021-01-31 09:12:57 +1100

    User data seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+print "Creating Users... "
+  User.destroy_all
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+u1 = User.create!(
+name: 'Jane Doe',
+email: 'jane@jane.com',
+password:  'chicken',
+dog_name: 'Huey',
+dog_breed: 'Labrador',
+vaccinated: 'true',
+vet: 'Wooferines',
+vaccination_certificate: "https://res.cloudinary.com/do9y9tnbr/image/upload/v1612003377/dogpark_vaccinations/Hueysvaccinations_mncxki.pdf",
+username: "labradore"
+)
